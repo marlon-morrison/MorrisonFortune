9c51e1d6207210a1d569a0f5097a585df28b561e
marlon-morrison <ssmorrison0@gmail.com>
2023-06-12 09:42:16 -0500

    Random Fortune

---
+++ MorrisonFortune.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import random
+fortune = random.randint(1,5)
+if fortune == 1:
+   print("Be on the alert to recognize your prime at whatever time of your life it may occur.")
+elif fortune == 2:
+   print("Your road to glory will be rocky, but fulfilling.")
+elif fortune == 3:
+   print("Courage is not simply one of the virtues, but the form of every virtue at the testing point.")
+elif fortune == 4:
+   print("The early bird gets the worm, but the second mouse gets the cheese.")
+else:
+   print("Nothing is impossible to a willing heart.")
